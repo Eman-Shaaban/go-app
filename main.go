7474c7f2e0dc031249742ea3b1a9acde70ba1664
Eman Shaaban <eman.sha3ban@gmail.com>
2024-11-02 01:27:11 +0200

    Initial

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Go away!")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", hello)
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func main() {
+	handleRequests()
+}
